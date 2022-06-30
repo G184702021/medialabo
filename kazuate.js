6b9e4085a564e3b2e228261667bf72afac380b32
G184702021 <218470@st.takushoku-u.ac.jp>
2022-06-30 15:16:11 +0900

    変更４－２

--- kazuate.js
+++ kazuate.js
@@ -7,12 +7,13 @@ let kaisu = 0;
 
 // 予想を4回実行する
 hantei();
-hantei();
-hantei();
-hantei();
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
+let p = document.querySelector('p#result');
+let result;
+result = document.createElement('result');
+
     let end = 0;
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
@@ -22,19 +23,24 @@ if(end < 1) {
     if (kaisu < 3) {
         if (kotae === yoso) {
             console.log('正解です．おめでとう!');
+            result.textContent = '正解です．おめでとう!';
             end = end+1;
         } else if (yoso < kotae) {
             console.log('まちがい．答えはもっと大きいですよ');
+            result.textContent = 'まちがい．答えはもっと大きいですよ';
         } else {
             console.log('まちがい．答えはもっと小さいですよ');
+            result.textContent = 'まちがい．答えはもっと小さいですよ';
         }
     } else {
         console.log('答えは'+kotae+'でした．すでにゲームは終わっています');
+        result.textContent = '答えは'+kotae+'でした．すでにゲームは終わっています';
     }
 }  else {
         console.log('答えは'+kotae+'でした．すでにゲームは終わっています');
+        result.textContent = '答えは'+kotae+'でした．すでにゲームは終わっています';
     }
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
-
+    p.insertAdjacentElement('beforeend', result);
 }
